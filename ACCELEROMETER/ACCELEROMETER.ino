30761b85f845fe1d8c7d4c6a41c1b84805f333fe
pmd-hfek2 <londonengineering@precisionmicrodrives.com>
2019-11-15 14:36:16 +0000

    basic accelerometer functionality

--- ACCELEROMETER/ACCELEROMETER.ino
+++ ACCELEROMETER/ACCELEROMETER.ino
@@ -13,29 +13,129 @@
 //--------------- Arduino pins -----------------------------
 
 //--- ACCELEROMETER:
-#define pinA  A2        //X axis
-#define pinB  A3        //Y axis
-#define pinB  A4        //Z axis
+#define pinX  A4        //X axis
+#define pinY  A2        //Y axis
+#define pinZ  A0        //Z axis
 
 
 //-------------- Variables ----------------------------------
+uint16_t X_read,Y_read,Z_read;      //10 bit accelerometer signal (raw)
+float X_G,Y_G,Z_G;                  //Accelerometer axis in G (gain)
+
+float X_gain,Y_gain,Z_gain;         //Accelerometer calibration factor (~80mV/G)
+int X_th,Y_th,Z_th;                 //Accelerometer threshold (noise)
+float X_max,Y_max,Z_max;            //G value
+
 
 
 //************************************ SETUP ******************************************
+/*
+ * CALIBRATE:
+ * Lay accelerometer flat, print values of 1 axis, record min value (threshold)
+ * Flip accelerometer, record values
+ * Calculate difference, this is the value of 1G (Gain is 1/difference)
+ * Divide reading of accelerometer flat by this difference, this is the bias factor
+ * The absolute value for axis in G is: Reading/Gain - Bias
+ * 
+ * Z: 165,198 --> 1G = 33 --> 1/33 = 0.0303 --> 198/33 = 6G ==> Z = (read*0.0303)-6
+ * Y: 165,198 --> 1G = 33 --> 1/33 = 0.0303 -->
+ * X: 167,200 --> 1G = 33 --> 1/33 = 0.0303
+*/
+
 void setup()
 {
+  Serial.begin(9600);
+  
+  //init (pins)
+  pinMode(pinX,INPUT);
+  pinMode(pinY,INPUT);
+  pinMode(pinZ,INPUT);
+
+  //init (gain, threshold)
+  X_th = 183;//182
+  X_gain = 0.0303; 
+  X_max = 0;
+  Y_th = 182; //182
+  Y_gain = 0.0303;
+  Y_max = 0;
+  Z_th = 198; //197
+  Z_gain = 0.0303;
+  Z_max = 0;
+  
 
 } //END: setup
 
 //******************************* LOOP ***************************************
 void loop()
 {
-
+  read_accel();
+  if(X_read > X_th && Y_read > Y_th && Z_read > Z_th)
+  {
+    //Serial.println(F("*** ACCEL. X-Y-Z (raw)   ***"));
+    Serial.println(F("*** ACCEL. X-Y-Z (G)     ***"));
+    while(X_read > X_th || Y_read > Y_th || Z_read > Z_th)
+    {
+      //print_raw();
+      print_accel();
+      read_accel();
+    }
+    print_max();
+  }
+  X_max = Y_max = Z_max = 0;
+  
 } //END: loop
 
 
 //************** FUNCTIONS ***********************
 void debug()
 {
-  
+  //DEBUG
+}
+
+void read_accel()
+{
+  X_read = analogRead(pinX);
+  Y_read = analogRead(pinY);
+  Z_read = analogRead(pinZ);
+
+  X_G = abs((X_read*X_gain)-6);
+  Y_G = abs((Y_read*Y_gain)-6);
+  Z_G = abs((Z_read*Z_gain)-6);
+
+  if(X_G > X_max)
+    X_max = X_G;
+  if(Y_G > Y_max)
+    Y_max = Y_G;
+  if(Z_G > Z_max)
+    Z_max = Z_G;
+}
+
+void print_raw()
+{
+  Serial.print(X_read);
+  Serial.print(F("   ; "));
+  Serial.print(Y_read);
+  Serial.print(F("   ; "));
+  Serial.println(Z_read);
+}
+
+void print_accel()
+{
+  Serial.print(X_G);
+  Serial.print(F("G ; "));
+  Serial.print(Y_G);
+  Serial.print(F("G ; "));
+  Serial.print(Z_G);
+  Serial.println(F("G"));
+}
+
+void print_max()
+{
+  Serial.println(F("*** ACCEL. X-Y-Z (Max. G) ***"));
+  Serial.print(X_max);
+  Serial.print(F("G ; "));
+  Serial.print(Y_max);
+  Serial.print(F("G ; "));
+  Serial.print(Z_max);
+  Serial.println(F("G"));
 }
