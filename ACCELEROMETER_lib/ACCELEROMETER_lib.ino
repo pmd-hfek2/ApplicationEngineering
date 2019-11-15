042dc2bc718ad0aa9f4c2370d8ce921274feb24e
pmd-hfek2 <londonengineering@precisionmicrodrives.com>
2019-11-15 10:30:22 +0000

    applied PMD template

--- ACCELEROMETER_lib/ACCELEROMETER_lib.ino
+++ ACCELEROMETER_lib/ACCELEROMETER_lib.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+//--- PRECISION MICRODRIVES ---
+//-- OPP 1066 - AddHaptics
+//- Tristan Cool
+//- November, 2019
 
-}
+//*** This program reads accelerometer axis***//
+//*** TODO ***//
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+//-------------- Libraries ----------------------------------
+#include "Accelerometer.h"
+
+//--------------- Arduino pins -----------------------------
+
+//--- ACCELEROMETER:
+#define pinA  A2        //X axis
+#define pinB  A3        //Y axis
+#define pinB  A4        //Z axis
+
+
+//-------------- Variables ----------------------------------
+
+
+//************************************ SETUP ******************************************
+void setup()
+{
+
+} //END: setup
+
+//******************************* LOOP ***************************************
+void loop()
+{
+
+} //END: loop
+
+
+//************** FUNCTIONS ***********************
+void debug()
+{
+  
 }
---
+++ ACCELEROMETER_lib/Accelerometer.h
@@ -0,0 +1,31 @@
+/*
+* Accelerometer class Arduino library for Precision Motordrives
+* Tristan Cool - November, 2019
+*/
+#ifndef Accelerometer_h
+#define Accelerometer_h
+
+#include <inttypes.h>
+
+class Accelerometer{
+	
+	public:
+	//Constructors:
+
+		
+	//Variables:
+
+				
+	//Functions:
+
+		
+	private:
+	//Pins:
+
+		
+	//Variables:
+
+		
+};
+
+#endif
